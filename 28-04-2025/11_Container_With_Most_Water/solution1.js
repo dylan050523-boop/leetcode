a2d586c8686ce37be70f373db16a6c3afd3e4dde
dylan0523 <168317670+dylan050523-boop@users.noreply.github.com>
2025-04-29 17:30:09 +0900

    Add files via upload

---
+++ 28-04-2025/11_Container_With_Most_Water/solution1.js
@@ -0,0 +1,46 @@
+// https://leetcode.com/problems/container-with-most-water/?envType=problem-list-v2&envId=array
+
+/*
+* You are given an integer array height of length n.
+* There are n vertical lines drawn such that
+* the two endpoints of the ith line are (i, 0) and (i, height[i]).
+*
+* Find two lines that together with the x-axis form a container,
+* such that the container contains the most water.
+*
+* Return the maximum amount of water a container can store.
+*
+* Notice that you may not slant the container.
+* */
+
+/*
+* n == height.length
+* 2 <= n <= 105
+* 0 <= height[i] <= 104
+* */
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    const countArray = [];
+    let maxHeight = 0;
+    for (let i = 0; i < height.length; i++) {
+        for (let j = i; j < height.length; j++) {
+            if (maxHeight < j) {
+                maxHeight = j;
+            }
+
+            if (height[i] < height[j] || j === height.length - 1) {
+                countArray.push(j)
+                break;
+            }
+        }
+        console.log(countArray);
+    }
+    console.log(maxHeight)
+};
+
+console.log(maxArea([1,8,6,2,5,4,8,3,7])); // 49
+console.log(maxArea([1, 1])); // 1
---
+++ 28-04-2025/11_Container_With_Most_Water/solution6.js
@@ -0,0 +1,60 @@
+// https://leetcode.com/problems/container-with-most-water/?envType=problem-list-v2&envId=array
+
+/*
+* You are given an integer array height of length n.
+* There are n vertical lines drawn such that
+* the two endpoints of the ith line are (i, 0) and (i, height[i]).
+*
+* Find two lines that together with the x-axis form a container,
+* such that the container contains the most water.
+*
+* Return the maximum amount of water a container can store.
+*
+* Notice that you may not slant the container.
+* */
+
+/*
+* n == height.length
+* 2 <= n <= 105
+* 0 <= height[i] <= 104
+* */
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    const countArray = [];
+    let maxHeight = 0;
+    let maxCount = 0;
+    for (let i = 0; i < height.length; i++) {
+        let count = 0;
+        for (let j = i + 1; j < height.length; j++) {
+            if (height[i] < height[j]) {
+                if (count !== 0) {
+                    count += 1;
+                }
+                break;
+            }
+
+            count += 1;
+        }
+
+        if (count > maxCount) {
+            maxCount = count;
+            maxHeight = height[i];
+        }
+
+        countArray.push(count);
+    }
+    console.log(countArray)
+    console.log(maxHeight)
+    // maxHeight = Math.max(...countArray);
+    return maxHeight ** 2
+};
+
+console.log(maxArea([1,8,6,2,5,4,8,3,7])); // 49
+console.log(maxArea([1, 1])); // 1
+console.log(maxArea([1, 2, 2]))
+
+console.log(maxArea([1, 2, 1]));
